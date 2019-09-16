727cb5bbd4636b01a84afc9b6eb078f2a6857fef
majinmaru <gotch11@naver.com>
2019-09-17 03:12:20 +0900

    190917
    틀은 다 잡은듯

--- mywork/jeep-web/javascripts/index.js
+++ mywork/jeep-web/javascripts/index.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-	$("body").niceScroll();
+	$("html").niceScroll();
+
+	var stickyHeader = function() {
+		var contentsTop = $(".contents").offset().top;
+		var scrollTop = $(window).scrollTop();
+		if(scrollTop > contentsTop){
+			$(".btn-wrapper").addClass("sticky");
+			//$(".container").css('margin-top', '-4.25rem');
+		} else {
+			$(".btn-wrapper").removeClass("sticky");
+			//$(".container").css('margin-top', '-8.25rem');
+		}
+	}
+
+	$(window).scroll(function() {
+		stickyHeader();
+	});
 });
