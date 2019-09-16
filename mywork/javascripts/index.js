6e12d69679ee2dde2134d59b53af88e26f7b9097
majinmaru <gotch11@naver.com>
2019-09-16 15:19:41 +0900

    190916 mywork

--- mywork/javascripts/index.js
+++ mywork/javascripts/index.js
@@ -1,57 +1,27 @@
 $(document).ready(function(){
-	/*
-	$('#uxui .worklist-card-wrapper').slick({
-	    centerMode: true,
-	    infinite: true,
-	    slidesToShow: 1,
-	    variableWidth: true,
-	    centerPadding: 0,
-	    prevArrow: $('#uxui .worklist-arrow__btn.prev'),
-	    nextArrow: $('#uxui .worklist-arrow__btn.next')
-	});
-
-	$('#promotion .worklist-card-wrapper').slick({
-	    centerMode: true,
-	    infinite: false,
-	    slidesToShow: 1,
-	    variableWidth: true,
-	    centerPadding: 0,
-	    prevArrow: $('#promotion .worklist-arrow__btn.prev'),
-	    nextArrow: $('#promotion .worklist-arrow__btn.next')
-	});
-	$('#graphic .worklist-card-wrapper').slick({
-	    centerMode: true,
-	    infinite: true,
-	    slidesToShow: 1,
-	    variableWidth: true,
-	    centerPadding: 0,
-	    prevArrow: $('#graphic .worklist-arrow__btn.prev'),
-	    nextArrow: $('#graphic .worklist-arrow__btn.next')
-	});
-*/
 
 	$('.worklist').eq(0).addClass('current');
 	$('.worktab-btn').eq(0).addClass('current');
-	$('.worklist').eq(0).find('.worklist-card-wrapper').slick({
+	$('#uxui-list .worklist-card-wrapper').slick({
 	    centerMode: true,
 	    infinite: true,
 	    slidesToShow: 1,
 	    variableWidth: true,
 	    centerPadding: 0,
-	    prevArrow: $('.worklist-arrow__btn.prev'),
-	    nextArrow: $('.worklist-arrow__btn.next')
+	    prevArrow: $('#uxui-list .worklist-arrow__btn.prev'),
+	    nextArrow: $('#uxui-list .worklist-arrow__btn.next')
 	});
 
 	$('.worktab-btn').click(function(e){
 		e.preventDefault();
 		var currentIndex = $(this).index();
 		var currentId = '#' + $(this).attr('id');
-		console.log(currentId);
 		$('.worktab-btn').eq(currentIndex).addClass('current');
 		$('.worktab-btn').eq(currentIndex).siblings().removeClass('current');
 
 		$('.worklist').eq(currentIndex).addClass('current');
 		$('.worklist').eq(currentIndex).siblings().removeClass('current');
+		/*
 		$(currentId + '-list').find('.worklist-card-wrapper').slick({
 			centerMode: true,
 		    infinite: true,
@@ -60,18 +30,26 @@ $(document).ready(function(){
 		    centerPadding: 0,
 		    prevArrow: $(currentId + '-list .worklist-arrow__btn.prev'),
 		    nextArrow: $(currentId + '-list .worklist-arrow__btn.next')
-		})
-		/*
-		$('.worklist').eq(currentIndex).find('.worklist-card-wrapper').slick({
-		    centerMode: true,
-		    infinite: true,
-		    slidesToShow: 1,
-		    variableWidth: true,
-		    centerPadding: 0,
-		    prevArrow: $('.worklist-arrow__btn.prev'),
-		    nextArrow: $('.worklist-arrow__btn.next')
 		});
-		$('.worklist').eq(currentIndex).siblings().find('.worklist-card-wrapper').slick('unslick');
 		*/
+		if($(currentId + '-list').find('.worklist-card-wrapper').hasClass('slick-initialized')){
+			return false;
+		} else {
+			$(currentId + '-list').find('.worklist-card-wrapper').slick({
+				centerMode: true,
+			    infinite: true,
+			    slidesToShow: 1,
+			    variableWidth: true,
+			    centerPadding: 0,
+			    prevArrow: $(currentId + '-list .worklist-arrow__btn.prev'),
+			    nextArrow: $(currentId + '-list .worklist-arrow__btn.next')
+			});
+		}
+	});
+
+	$('.worklist-card').hover(function(){
+		$('.card-detail__line').addClass('hover');
+	}, function(){
+		$('.card-detail__line').removeClass('hover');
 	})
 });
