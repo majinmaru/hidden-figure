e7a8b34ec898adcffbe3b164642cbbd8c1f48143
majinmaru <gotch11@naver.com>
2019-10-07 14:12:03 +0900

    191007
    jlc started

--- mywork/jlc/javascripts/index.js
+++ mywork/jlc/javascripts/index.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-	
+	$("html").niceScroll();
+
+	var stickyHeader = function() {
+		var contentsTop = $(".contents").offset().top;
+		var scrollTop = $(window).scrollTop();
+		if(scrollTop > contentsTop){
+			$(".btn-wrapper").addClass("sticky");
+		} else {
+			$(".btn-wrapper").removeClass("sticky");
+		}
+	}
+
+
+	$(window).scroll(function() {
+		stickyHeader();
+	});
 });
