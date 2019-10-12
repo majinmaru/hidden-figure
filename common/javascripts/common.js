18524bd8095a883a8d045f84aed4507a2842cfc8
majinmaru <gotch11@naver.com>
2019-10-12 19:00:49 +0900

    191012
    top버튼 추가

--- common/javascripts/common.js
+++ common/javascripts/common.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
 	
+	var topbtnAppear = function() {
+		var windowHeight = $(window).height();
+		var scrollTop = $(window).scrollTop();
+
+		if(scrollTop > windowHeight){
+			$(".topbtn").addClass("appear");
+		} else {
+			$(".topbtn").removeClass("appear");
+		}
+	}
+
+	$(".topbtn").click(function(e){
+		e.preventDefault();
+		//$(window).scrollTop(0);
+		$('html, body').animate({
+            scrollTop : 0
+        }, 1000);
+	})
+
+	$(window).scroll(function() {
+		topbtnAppear();
+	});
 });
